e5cf5a1dfddb4e664284acbb3e592b3eea5ac7be
Srcy Dev <88575889+SrcyDev@users.noreply.github.com>
2022-05-27 11:24:44 +0530

    Update and rename main.py to automate-confirm.py

--- automate-confirm.py
+++ automate-confirm.py
@@ -1,12 +1,21 @@
 ######################################## WELCOME TO AUTOMATION SCRIPT############################################################
 ## This is a template for your automating script. You can use this to execute things automatically by launching it in startup. ##
-## This is made and tested for Linux. This may work on other Operating System but I can not gurrantee it.                      ##
+## This is made and tested for Linux. This may work but I can not gurrantee it.                                                ##
 ## I will appreciate any help. I hope you will enjoy it.                                                                       ##
 ################################################################################################################################# 
 
-# !/usr/bin/env python
+## Enter your commands here like command = os.system(<program name>)
+## Copy and paste this if you want to use multiple programs.
+## Make sure they are present in /usr/bin or are added to path.
+## Else it would not work.
 
-#### START SCREEN ####
+def cmd():
+	command = os.system()
+
+
+
+## Dont edit this unless you know what are you doing
+
 print('''
 ░█████╗░██╗░░░██╗████████╗░█████╗░███╗░░░███╗░█████╗░████████╗██╗░█████╗░███╗░░██╗
 ██╔══██╗██║░░░██║╚══██╔══╝██╔══██╗████╗░████║██╔══██╗╚══██╔══╝██║██╔══██╗████╗░██║
@@ -23,16 +32,10 @@ print('''
 ╚═════╝░░╚════╝░╚═╝░░╚═╝╚═╝╚═╝░░░░░░░░╚═╝░░░ ''')
 
 
-
 import os
 import time
 import sys
 
-## Enter the name of the program in the list and
-## make sure it is present in /use/bin.
-cmd = ["","",""]
-
-#####Don't edit this unless you know what are you doing.####
 
 ## This runs one time only. In order to use it again the next function is defined.
 ## I was not able to loop it. I will gladly accept any help about this.
@@ -54,19 +57,16 @@ def loading_text():
         	sys.stdout.flush()
 	print
 
-## This is where your command is executed.
-## Use this like "command = os.system("<file>")
-## Add multiple lines for multiple files you want to execute
-
-
-def cmd():
-	command = os.system(cmd) 
-
 ## Input
-## This checks for any possible input to prevent errors. 
+## This checks for any possible input to prevent errors.
+## This done if you somehow misclick or put a wrong name 
+## which can lead to errors of some kind.
+## Use the no-confirm script if you do not want this.
+
 if startup_prompt == "yes" or startup_prompt == "ye" or startup_prompt == "y":
 	print("\u001b[33mStarting...")
 	loading_text()
+	print("\n")
 	cmd()
 	print("\n\u001b[32mDone.... ")
 	print("\u001b[32mOperation Succesful\u001b[0m")
